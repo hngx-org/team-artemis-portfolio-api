207932f9c8b7a99bac577f979452af67f975ffbf
Ubong Edem <ubongedem78@gmail.com>
2023-10-07 19:17:41 +0100

    Model Setup && Folder Re-structure

---
+++ src/data-source.ts
@@ -0,0 +1,19 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import * as entities from "./entity/model";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  logging: false,
+  entities: Object.keys(entities),
+  migrations: [],
+  subscribers: [],
+});
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,22 @@
-import app from "./app";
+import { AppDataSource } from "./data-source";
+import express from "express";
+import greetingRoute from "./routes/greeting.route";
 
-const port = process.env.PORT || 3000;
+AppDataSource.initialize()
+  .then(async () => {
+    const app = express();
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+    // middleware setup
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    // route setup
+    app.use("/", greetingRoute);
+
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => console.log(error));
