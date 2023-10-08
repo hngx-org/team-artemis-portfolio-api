2feb0587e1fd5e1542f85bf39604a5b18e9ffe8d
Sn0wF1re <mokayaclinton34@gmail.com>
2023-10-08 15:30:16 +0300

    Edit test for contacts

--- tests/contact.spec.ts
+++ tests/contact.spec.ts
@@ -6,11 +6,17 @@ const request = supertest(app);
 describe('Contact API endpoints', () => {
   let contactId: number;
   // Endpoint to Create Contact section
-  describe('POST api/contact', () => {
+  describe('POST api/contacts', () => {
     it('should create Contact section', async () => {
+      const socialUsers = await request.get('api/social-user');
+      const { url, user_id, social_media_id } = socialUsers.body[0];
       const res = await request
-        .post('api/contact')
-        .send({ name: 'Contact', description: 'This is the contact section' });
+        .post('api/contacts')
+        .send({
+          url,
+          userId: user_id,
+          socialMediaId: social_media_id
+        });
       expect(res.status).to.equal(201);
       expect(res.body).to.be.an('object');
       contactId = res.body.id;
@@ -18,20 +24,20 @@ describe('Contact API endpoints', () => {
   });
 
   // Endpoint to fetch Contact section
-  describe('Get api/contact', () => {
+  describe('Get api/contacts', () => {
     it('should fetch contact section', async () => {
       const res = await request
-        .get('api/contact');
+        .get('api/contacts');
       expect(res.status).to.equal(200);
       expect(res.body).to.be.an('object');
     });
   });
 
   // Endpoint to update Contact section
-  describe('PUT api/contact/:contactId', () => {
+  describe('PUT api/contacts/:contactId', () => {
     it('should update Contact section', async () => {
       const res = await request
-        .put(`api/contact/${contactId}`) 
+        .put(`api/contacts/${contactId}`) 
         .send({ description: 'Another contact description' });
       expect(res.status).to.equal(200);
       expect(res.body).to.be.an('object');
@@ -39,14 +45,14 @@ describe('Contact API endpoints', () => {
   });
 
   // Endpoint to Delete Contact section
-  describe('DELETE api/contact/:contactId', () => {
+  describe('DELETE api/contacts/:contactId', () => {
     it('should delete Contact section', async () => {
-      const initialContacts = await request.get('api/contact');
+      const initialContacts = await request.get('api/contacts');
       const res = await request
-        .delete(`api/contact/${contactId}`);
+        .delete(`api/contacts/${contactId}`);
       expect(res.status).to.equal(204);
       // Verify that the section is deleted in the database
-      const updatedContacts = await request.get('api/contact');
+      const updatedContacts = await request.get('api/contacts');
       expect(updatedContacts.body.length).to.equal(initialContacts.body.length - 1);
     });
   });
