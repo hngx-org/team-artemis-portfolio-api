dae2dc2883ca9a11605c2cf192596762b60ca04c
kingsley-sunny <ezeobisunny51@gmail.com>
2023-10-08 13:17:18 +0100

    Implmented the create portfolio profile details endpoint

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -1,11 +1,11 @@
-import express, { Request, RequestHandler, Response } from "express";
-import { error, success } from "../utils";
+import { Request, RequestHandler, Response } from "express";
+import { connectionSource } from "../database/data-source";
+import { PortfolioDetails, UserTrack } from "../database/entity/model";
+import { User } from "../database/entity/user";
 import { cloudinaryService, uploadProfileImageService } from "../services";
+import { error, success } from "../utils";
 
-export const uploadProfileImageController: RequestHandler = async (
-  req: Request,
-  res: Response
-) => {
+export const uploadProfileImageController: RequestHandler = async (req: Request, res: Response) => {
   try {
     if (!req.files) return error(res, "add event image", 400);
     const { service, userId } = req.body;
@@ -15,5 +15,42 @@ export const uploadProfileImageController: RequestHandler = async (
     return success(res, data, "Profile picture uploaded successfully");
   } catch (err) {
     error(res, (err as Error).message); // Use type assertion to cast 'err' to 'Error' type
+  }
+};
+
+export const createProfileController = async (req: Request, res: Response) => {
+  try {
+    const { name, trackId, city, country } = req.body;
+    const userId = req.params.userId;
+
+    const userRepository = connectionSource.getRepository(User);
+    const portfolioDetailsRepository = connectionSource.getRepository(PortfolioDetails);
+    const userTrackRepository = connectionSource.getRepository(UserTrack);
+
+    const user = await userRepository.findOne({ where: { id: userId } });
+
+    if (!user) {
+      return error(res, "User Not found", 400);
+    }
+
+    if (name) {
+      userRepository.update(user.id, { lastName: name });
+    }
+
+    const userTrack = userTrackRepository.findOne({ where: { trackId: trackId, userId } });
+
+    if (!userTrack) {
+      const newUser = userTrackRepository.create({ trackId: trackId, userId });
+
+      await userTrackRepository.save(newUser);
+    }
+
+    const portfolio = portfolioDetailsRepository.create({ city, country, userId });
+
+    await portfolioDetailsRepository.save(portfolio);
+
+    return success(res, portfolio, "Successfully Created Portfolio profile");
+  } catch (err) {
+    return error(res, err.message, 500);
   }
 };
--- src/routes/profile.route.ts
+++ src/routes/profile.route.ts
@@ -1,7 +1,7 @@
 // this is an example file
 import express from "express";
 import multer from "multer";
-import { uploadProfileImageController } from "../controllers";
+import { createProfileController, uploadProfileImageController } from "../controllers";
 
 const storage = multer.memoryStorage();
 const uploads = multer({ storage }).array("images", 1);
@@ -47,5 +47,53 @@ const router = express.Router();
  *     multipart: true
  */
 router.post("/profile/image/upload", uploads, uploadProfileImageController);
+
+/**
+ * @swagger
+ * /profile/{userId}:
+ *   put:
+ *     summary: Create Portfolio profile
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         description: The userId.
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       description: New profile detail data
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               field1:
+ *                 type: string
+ *               field2:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *     tags:
+ *       - Education
+ */
+router.post("/profile/:userId", createProfileController);
 
 module.exports = router;
