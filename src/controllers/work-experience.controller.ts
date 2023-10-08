f6de590d03472834e02a07f1b59f1e52224cbaa0
abdullmustyy <abdulmusty03@outlook.com>
2023-10-08 10:36:48 +0100

    Feat: Implement create work experience endpoint to test the delete endpoint

--- src/controllers/work-experience.controller.ts
+++ src/controllers/work-experience.controller.ts
@@ -1,15 +1,65 @@
 import { RequestHandler } from "express";
 import { error, success } from "../utils";
-import { deleteWorkExperienceServise } from "../services/work-experience.service";
+import { connectionSource } from "../database/data-source";
+import { WorkExperienceDetail } from "../database/entity/model";
+
+// Define a Data Transfer Object (DTO) for adding WorkExperienceDetail
+export interface WorkExperienceDTO {
+  role?: string;
+  company?: string;
+  description?: string;
+  startMonth?: string;
+  startYear?: string;
+  endMonth?: string;
+  endYear?: string;
+  isEmployee?: boolean;
+  userId?: string;
+  sectionId?: number;
+}
+
+// Get the repository for the WorkExperienceDetail entity
+const workExperienceRepository =
+  connectionSource.getRepository(WorkExperienceDetail);
+
+export const addWorkExperience: RequestHandler = async (req, res, next) => {
+  try {
+    // Validate and apply updates from the DTO
+    const workExperienceData = req.body as WorkExperienceDTO;
+
+    // Create a new work experience detail
+    const workExperience = workExperienceRepository.create(workExperienceData);
+
+    // Save the new work experience detail
+    const data = await workExperienceRepository.save(workExperience);
+
+    success(res, data, "Work Experience Added");
+  } catch (err) {
+    console.log(err);
+    error(res, (err as Error).message);
+  }
+};
 
 export const deleteWorkExperience: RequestHandler = async (req, res, next) => {
   try {
-    const { id } = req.params as unknown as { id: number };
+    // Convert the ID to a number
+    const id = parseInt(req.params.id);
 
-    const data = await deleteWorkExperienceServise(id);
+    // Find the existing work experience detail by ID
+    const workExperienceToRemove = await workExperienceRepository.findOneBy({
+      id: id,
+    });
+
+    // If the work experience detail doesn't exist, return a 404 Not Found
+    if (!workExperienceToRemove) {
+      return error(res, "Work Experience not found", 404);
+    }
+
+    // Delete the work experience detail
+    const data = await workExperienceRepository.remove(workExperienceToRemove);
 
     success(res, data, "Work Experience Deleted");
   } catch (err) {
+    console.log(err);
     error(res, (err as Error).message);
   }
 };
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,4 @@
 export * from "./greeting.service";
 export * from "./image-upload.service";
 export * from "./profile.service";
-export * from "./skills.service"
-export * from "./work-experience.service";
+export * from "./skills.service";
