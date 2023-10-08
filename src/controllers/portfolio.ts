7df8e17bea8c06196f844b2710ee1ed84eab5ddb
Samuel Ikoli <98848723+samuelIkoli@users.noreply.github.com>
2023-10-08 10:15:32 +0100

    get all portfolios and get portfolio

---
+++ src/controllers/portfolio.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { connectionSource } from "../database/data-source";
+import { User } from "../database/entity/user";
+import { UserTrack, PortfolioDetails } from "../database/entity/model";
+
+const userRepository = connectionSource.getRepository(User);
+const portfolioRepository = connectionSource.getRepository(PortfolioDetails);
+const trackRepository = connectionSource.getRepository(UserTrack);
+
+const getAllUsers = async (req: Request, res: Response) => {
+    const users = await userRepository.find();
+    res.json(users);
+}
+
+const getUserById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const user = await userRepository.findOne({ where: { id } });
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(404).json({ message: "User not found" });
+    }
+}
+
+export { getAllUsers, getUserById };
