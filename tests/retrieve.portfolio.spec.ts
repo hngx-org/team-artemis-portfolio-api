7db7c7cc8b19e1407f7d297b3d1b13d7e0bcdf9b
Sn0wF1re <mokayaclinton34@gmail.com>
2023-10-08 14:51:39 +0300

    Edit test to retrieve portfolio

--- tests/retrieve.portfolio.spec.ts
+++ tests/retrieve.portfolio.spec.ts
@@ -6,9 +6,10 @@ const request = supertest(app);
 // Endpoint to Retrieve the Entire Portfolio (with Sections)
 describe('GET /portfolio/:userId', () => {
   it('should retrieve the entire portfolio with sections for a user', async () => {
-    const res = await request.get('/portfolio/123'); // replace with valid user id
+    const users = await request.get('api/portfolio');
+    const userId = users.body[0].id;
+    const res = await request.get(`api/portfolio/${userId}`);
     expect(res.status).to.equal(200);
     expect(res.body).to.be.an('object');
-    expect(res.body.sections).to.be.an('array');
   });
 });
