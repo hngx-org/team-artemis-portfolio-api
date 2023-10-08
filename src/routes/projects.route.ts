dabc0a0343f16160529c111687f2446720968030
Erickb <wr.t8r1@gmai.com>
2023-10-08 05:05:17 -0700

    Fix bug

--- src/routes/projects.route.ts
+++ src/routes/projects.route.ts
@@ -13,7 +13,60 @@ import {
     deleteProjectById,
 } from "../controllers/projects.controller";
 
-
+/**
+ * @swagger
+ * /projects/{id}:
+ *   put:
+ *     summary: Update project by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the project for update.
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       description: Update details for the project
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               year:
+ *                 type: number
+ *               url:
+ *                  type: string
+ *               tags:
+ *                   type: string
+ *               description:
+ *                    type: string
+ *               userId: 
+ *                     
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       400:
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *     tags:
+ *       - Education
+ */
 
 router.get("/projects", getAllProjects)
 router.get("/projects/:id", getProjectById);
