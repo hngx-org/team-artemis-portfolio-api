8e0e46c4188f194eeba9d5e7867dac3ca10ceebb
abdullmustyy <abdulmusty03@outlook.com>
2023-10-08 12:19:18 +0100

    Chore: Remove endpoint to create work experience

--- src/controllers/work-experience.controller.ts
+++ src/controllers/work-experience.controller.ts
@@ -3,41 +3,9 @@ import { error, success } from "../utils";
 import { connectionSource } from "../database/data-source";
 import { WorkExperienceDetail } from "../database/entity/model";
 
-// Define a Data Transfer Object (DTO) for adding WorkExperienceDetail
-export interface WorkExperienceDTO {
-  role?: string;
-  company?: string;
-  description?: string;
-  startMonth?: string;
-  startYear?: string;
-  endMonth?: string;
-  endYear?: string;
-  isEmployee?: boolean;
-  userId?: string;
-  sectionId?: number;
-}
-
 // Get the repository for the WorkExperienceDetail entity
 const workExperienceRepository =
   connectionSource.getRepository(WorkExperienceDetail);
-
-export const addWorkExperience: RequestHandler = async (req, res, next) => {
-  try {
-    // Validate and apply updates from the DTO
-    const workExperienceData = req.body as WorkExperienceDTO;
-
-    // Create a new work experience detail
-    const workExperience = workExperienceRepository.create(workExperienceData);
-
-    // Save the new work experience detail
-    const data = await workExperienceRepository.save(workExperience);
-
-    success(res, data, "Work Experience Added");
-  } catch (err) {
-    console.log(err);
-    error(res, (err as Error).message);
-  }
-};
 
 export const deleteWorkExperience: RequestHandler = async (req, res, next) => {
   try {
--- src/routes/work-experience.route.ts
+++ src/routes/work-experience.route.ts
@@ -1,51 +1,9 @@
 import express from "express";
 import {
-  addWorkExperience,
   deleteWorkExperience,
 } from "../controllers/work-experience.controller";
 
 export const router = express.Router();
-
-/**
- * @swagger
- * /work-experience:
- *   post:
- *     summary: Create a work experience section.
- *     requestBody:
- *       description: New work experience section data.
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               field1:
- *                 type: string
- *               field2:
- *                 type: number
- *     responses:
- *       200:
- *         description: Successful response
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *       400:
- *         description: Bad request
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- *     tags:
- *       - Education
- */
-router.post("/work-experience", addWorkExperience);
 
 /**
  * @swagger
