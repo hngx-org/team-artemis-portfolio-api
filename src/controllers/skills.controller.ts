6453bcfd1cdd785b7970b8fe533f26e7b10d58bd
Sofiyyah <sofiyyahabidoye@gmail.com>
2023-10-08 11:47:12 +0100

    working on update and delete skills

--- src/controllers/skills.controller.ts
+++ src/controllers/skills.controller.ts
@@ -1,5 +1,9 @@
 import express, { Request, RequestHandler, Response } from "express";
-import { createSkillsService } from "../services/skills.service";
+import {
+  createSkillsService,
+  updateSkillsService,
+  deleteSkillsService,
+} from "../services/skills.service";
 import { error, success } from "../utils";
 
 export const createSkills: RequestHandler = async (
@@ -19,6 +23,40 @@ export const createSkills: RequestHandler = async (
     success(res, data);
   } catch (err) {
     error(res, (err as Error).message); // Use type assertion to cast 'err' to 'Error' type
+  }
+};
+
+export const updateSkills: RequestHandler = async (req: Request, res: Response) => {
+  try {
+    const id = parseInt(req.params.id);
+    const { skills, sectionId, userId } = req.body;
+
+    const data = await updateSkillsService(id, { skills, sectionId, userId });
+    if (data.successful) {
+      success(res, data);
+    } else {
+      error(res, data.message);
+    }
+  } catch (err) { 
+    error(res, (err as Error).message);
+  }
+}
+
+export const deleteSkills: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const id = parseInt(req.params.id);
+
+    const data = await deleteSkillsService(id);
+    if (data.successful) {
+      success(res, data);
+    } else {
+      error(res, data.message);
+    }
+  } catch (err) {
+    error(res, (err as Error).message);
   }
 };
 
--- src/routes/skills.route.ts
+++ src/routes/skills.route.ts
@@ -1,8 +1,13 @@
 import express from "express";
-import { createSkills } from "../controllers/skills.controller";
+import {
+  createSkills,
+  updateSkills,
+  deleteSkills,
+} from "../controllers/skills.controller";
 
 const router = express.Router();
 
 router.post("/create-skills", createSkills);
-
+router.put("/update-skills/:id", updateSkills);
+router.delete("/delete-skills/:id", deleteSkills);
 module.exports =  router;
