c473f66c5587daaa87a3d708f7864712305fd03b
Luther1921 <mota4real@gmail.com>
2023-10-08 15:55:08 +0100

    delete portfolio

--- src/controllers/portfolio.ts
+++ src/controllers/portfolio.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, RequestHandler, Response } from "express";
 import { connectionSource } from "../database/data-source";
 import { User } from "../database/entity/user";
 import { UserTrack, PortfolioDetails } from "../database/entity/model";
@@ -8,19 +8,47 @@ const portfolioRepository = connectionSource.getRepository(PortfolioDetails);
 const trackRepository = connectionSource.getRepository(UserTrack);
 
 const getAllUsers = async (req: Request, res: Response) => {
-    const users = await userRepository.find();
-    res.json(users);
-}
+  const users = await userRepository.find();
+  res.json(users);
+};
 
 const getUserById = async (req: Request, res: Response) => {
-    try {
-        const { id } = req.params;
-        const user = await userRepository.findOne({ where: { id } });
-        const portfolio = await portfolioRepository.findOne({ where: { userId: id } });
-        res.status(200).json({ user, portfolio });
-    } catch (error) {
-        res.status(404).json({ message: "User not found" });
-    }
-}
+  try {
+    const { id } = req.params;
+    const user = await userRepository.findOne({ where: { id } });
+    const portfolio = await portfolioRepository.findOne({
+      where: { userId: id },
+    });
+    res.status(200).json({ user, portfolio });
+  } catch (error) {
+    res.status(404).json({ message: "User not found" });
+  }
+};
 
-export { getAllUsers, getUserById };
+// deleting portfolio by id endpoint
+
+const deletePortfolio: RequestHandler = async (req: Request, res: Response) => {
+  try {
+    // convert the id to number
+    const id = parseInt(req.params.id);
+
+    // find the porfolio by id
+    const portfolioToRemove = await portfolioRepository.findOneBy({ id: id });
+
+    // return error if porfolio is not found
+    if (!portfolioToRemove) {
+      return res.status(404).json({ error: "Portfolio not found!" });
+    }
+
+    // delete the porfolio
+
+    const portfolio = await portfolioRepository.remove(portfolioToRemove);
+    res
+      .status(200)
+      .json({ message: "Portfolio deleted successfully", portfolio });
+  } catch (error) {
+    res.status(500).json(error as Error);
+  }
+};
+
+export { getAllUsers, getUserById, deletePortfolio };
--- src/routes/portfolio.ts
+++ src/routes/portfolio.ts
@@ -1,8 +1,11 @@
-import { getAllUsers, getUserById } from "../controllers/portfolio"
+import {
+  getAllUsers,
+  getUserById,
+  deletePortfolio,
+} from "../controllers/portfolio";
 import { Router } from "express";
 
-const router = Router();   // Create a new router
-
+const router = Router(); // Create a new router
 
 router.get("/portfolio", getAllUsers);
 
@@ -29,4 +32,51 @@ router.get("/portfolio", getAllUsers);
  */
 router.get("/portfolio/:id", getUserById);
 
-module.exports = router;  // Export the router
+// delete portfolio
+router.delete("/portfolio/:id", deletePortfolio);
+
+/**
+ * @swagger
+ * /portfolio/:id:
+ *   delete:
+ *     summary: Delete a Portfolio section.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of Portfolio to delete.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       404:
+ *         description: Not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *       500:
+ *         description: Internal error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *     tags:
+ *       - Portfolio
+ */
+
+module.exports = router; // Export the router
