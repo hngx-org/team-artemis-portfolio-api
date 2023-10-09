e6065d2433adc22e37154c0cc38bea0beb65b60e
Kmart002 <128054511+Kmart002@users.noreply.github.com>
2023-10-09 17:09:16 +0100

    add swagger docs

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ readdirSync("./src/routes").map((path) =>
 
 app.get("/", sayHelloController);
 
-const port = process.env.PORT || 3006;
+const port = process.env.PORT || 8008;
 
 app.listen(port, () => {
   // console.log(entities);
