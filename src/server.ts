66d45cf7bdcd341dd50b2c59d8daba0051c9b18c
kingsley-sunny <ezeobisunny51@gmail.com>
2023-10-11 00:55:04 +0100

    Added cors package

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import { connectionSource } from "./database/data-source";
+import cors from "cors";
 import express from "express";
-import * as entities from "./database/entity/model";
 import { readdirSync } from "fs";
 import { sayHelloController } from "./controllers/greeting.controller";
+import { connectionSource } from "./database/data-source";
 const swaggerUi = require("swagger-ui-express");
 const swaggerOptions = require("./swagger");
 const router = require("./routes/image-upload.route");
@@ -14,16 +14,15 @@ connectionSource
   .then(async () => {
     console.log("Database Connected");
   })
-  .catch((error) => console.log(error));
+  .catch(error => console.log(error));
 
 // middleware setup
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //serve all routes dynamically using readdirsync
-readdirSync("./src/routes").map((path) =>
-  app.use("/api", require(`./routes/${path}`))
-);
+readdirSync("./src/routes").map(path => app.use("/api", require(`./routes/${path}`)));
 app.get("/", sayHelloController);
 
 const port = process.env.PORT || 8008;
