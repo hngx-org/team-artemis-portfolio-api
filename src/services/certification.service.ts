84e1d11f9a92f286f876a412effaedd4e8522476
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2023-10-11 17:30:04 +0100

    Update certification service

---
+++ src/services/certification.service.ts
@@ -0,0 +1,33 @@
+import { connectionSource } from '../database/data-source'
+import { Certification } from '../database/entity/model'
+import { CertificationInterface } from '../interfaces/cerification.interface'
+
+export const updateCertification = async (
+   id: number,
+   userId: string,
+   payload: CertificationInterface
+) => {
+   try {
+      const certificationRepository = connectionSource.getRepository(Certification)
+
+      let certificateToUpdate = await certificationRepository.findOne({
+         where: {id, userId}
+      })
+
+      if (!certificateToUpdate) {
+         return { successful: false, message: "Certification not found" };
+      }
+
+      const {title, year, organization, url, description} = payload
+      if (!title || !year || !organization || !url || !description) {
+         return { successful: false, message: "Missing required field" }
+      }
+
+      certificateToUpdate = {...payload, id, userId}
+      await certificationRepository.save(certificateToUpdate);
+      return { successful: true, message: "Certification updated successfully" };
+   } catch (error) {
+      console.error('Error updating Certification', error.message)
+      throw new Error('Error updating Certification')
+   }
+}
