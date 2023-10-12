d51b9df5506b0054b7dcb8203be066b053f6fc0b
Kmart002 <128054511+Kmart002@users.noreply.github.com>
2023-10-12 09:05:10 +0100

    award

--- src/controllers/award.controller.ts
+++ src/controllers/award.controller.ts
@@ -1,8 +1,55 @@
-import { Request, Response } from 'express'
-import { connectionSource as dataSource } from '../database/data-source'
+import { Request, Response } from 'express';
+import { connectionSource as dataSource } from '../database/data-source';
+import { Award} from '../database/entity/model';
 
 const createAwardsController = () => {
 
 }
 
-export { createAwardsController }
+
+// Get award by Id
+ const getAwardController = async (req: Request, res: Response) => {
+    const awardRepo = dataSource.getRepository(Award)
+    
+    try {
+        const id = parseInt(req.params.id)
+        const award = await awardRepo.findOne({ 
+            where:{ id} });
+        if (!award) {
+            return res.status(404).json({message: 'Award not found'});
+        }
+        res.status(200).json({
+            message: 'Award created successfully',
+            award
+        })
+    } catch (error) {
+        console.error('Error getting award', error);
+        res.status(500).json({message: 'Internal server error'})
+    }
+    
+};
+    //Delete award by id
+ const deleteAwardController = async (req: Request, res: Response) => {
+    const awardRepo = dataSource.getRepository(Award)
+    
+    try {
+        const id = parseInt(req.params.id)
+        const award = await awardRepo.findOne({ 
+            where:{ id} });
+        if (!award) {
+            return res.status(404).json({message: 'Award not found'});
+        }
+        res.status(200).json({
+            message: 'Award deleted successfully',
+            award
+        })
+    } catch (error) {
+        console.error('Error deleting award', error);
+        res.status(500).json({message: 'Internal server error'})
+    }
+    
+};
+    export { createAwardsController,
+            getAwardController,
+            deleteAwardController
+           }
--- src/routes/award.route.ts
+++ src/routes/award.route.ts
@@ -1,8 +1,116 @@
 import express from 'express'
-import { createAwardsController } from '../controllers/award.controller'
+import { createAwardsController,
+        getAwardController,
+        deleteAwardController
+} from '../controllers/award.controller'
 
 const router = express.Router()
 
-router.post('awards/:id', createAwardsController)
+router.post('awards/:id', createAwardsController);
+
+/**
+ * @swagger
+ * /api/awards/:id:
+ *   get:
+ *     summary: Get award detail(s) for a user who's id is in the params and returns an array of objects containing a user award details.
+ *     description: Get award detail(s) for a user who's id is in the params and returns an array of objects containing a user award details.
+ *     tags: [Award]
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         type: string
+ *         description: Optional authorization header
+ *         schema:
+ *           type: array
+ *           items:
+ *             type: object
+ *             properties:
+ *               userId:
+ *                 type: string
+ *               title:
+ *                 type: string
+ *               year:
+ *                 type: string
+ *               presented_by:
+ *                 type: string
+ *               url:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               createdAt:
+ *                 type: Date
+ *               user:
+ *                 type: user
+ *               section:
+ *                  type: section
+ *
+ *     responses:
+ *       200:
+ *         description: Award created successfully.
+ *         educationDetails: Array of user awards detail(s).
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 successful:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     successful:
+ *                       type: boolean
+ *                     message:
+ *                       type: string    
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 successful:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: null
+ */
+router.get('/awards/:id', getAwardController);
+
+/**
+ * @swagger
+ * /api/awards/:id:
+ *   get:
+ *     summary: Delete award detail(s) of a user detail by ID
+ *     description: Delete award detail(s) of a user detail by ID
+ *     tags: [Award]
+ *     parameters:
+ *       in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the award detail to delete.
+ *         schema:
+ *           type: integer
+ *
+ *     responses:
+ *       200:
+ *         description: Award deleted successfully.
+ *         Details: Array of user awards detail(s).   
+ *  
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ */
+
+router.delete('/awards/:id', deleteAwardController)
 
 module.exports = router
