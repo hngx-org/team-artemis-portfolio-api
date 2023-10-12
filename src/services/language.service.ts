a6fa79b188bddc5e638c5420cd4baa9f1ec0440f
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-10-12 13:52:03 +0100

    updated endpoints after discussion with FE

--- src/services/language.service.ts
+++ src/services/language.service.ts
@@ -3,7 +3,7 @@ import { Response } from 'express';
 export const success = (res: Response, data: string | Object) => {
   return res.status(200).json({
     status: 200,
-    message: 'Sucess',
+    message: 'success',
     data: data,
   });
 };
