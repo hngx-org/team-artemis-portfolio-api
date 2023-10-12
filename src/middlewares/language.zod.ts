43bc37ed7d0cff19cecc761102e18269dc5cb7cb
Ononiwu Wisdom <ononiwuwisdom2@gmail.com>
2023-10-12 13:57:27 +0100

    updated endpoints after discussion with FE

--- src/middlewares/language.zod.ts
+++ src/middlewares/language.zod.ts
@@ -23,6 +23,7 @@ export const validateSchema =
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       await schema.parseAsync(req.body);
+
       return next();
     } catch (error: any) {
       return res.status(400).json({
