891688bafec1d4575adb56af9f12d0db906a4f81
abdullmustyy <abdulmusty03@outlook.com>
2023-10-12 13:18:47 +0100

    Feat: Implement endpoint to update and delete interest

--- src/routes/interests.route.ts
+++ src/routes/interests.route.ts
@@ -1,7 +1,17 @@
 import express from "express";
 import { z } from "zod";
-import { createInterest, getInterests } from "../controllers/interests.controller";
+import {
+  createInterest, getInterests,
+  updateInterest,
+  deleteInterest,
+} from "../controllers/interests.controller";
 import { validateSchema } from "../middlewares/custom.zod";
+import {
+  validateUpdateInterest,
+  validateUserId,
+  updateInterestsSchema,
+  userIdSchema,
+} from "../middlewares/interests.zod";
 
 const router = express.Router();
 
@@ -80,5 +90,135 @@ const interestSchema = z.object({
 
 router.post("/interests", validateSchema(interestSchema), createInterest);
 router.get("/interests/:userId", getInterests);
+
+// Update interests
+
+/**
+ * @swagger
+ * /api/interests/{userId}:
+ *   put:
+ *     summary: Update user interests.
+ *     description: Update the interests of a user.
+ *     tags:
+ *       - Interests
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         description: The ID of the user whose interests are to be updated.
+ *         required: true
+ *         schema:
+ *           type: string
+ *         example: "550e8400-e29b-41d4-a716-446655440000"
+ *       - in: body
+ *         name: UpdateDetails
+ *         description: Data for updating user interests.
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             interests:
+ *               type: array
+ *               items:
+ *                 type: string
+ *               example: ["Teaching", "Technology"]
+ *     responses:
+ *       200:
+ *         description: Interests details successfully updated.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 successful:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     interestArray:
+ *                       type: array
+ *                       items:
+ *                         type: string
+ *       500:
+ *         description: Failed to update interests.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 successful:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "Could not update interests."
+ *                 error:
+ *                   type: string
+ */
+
+router.put(
+  "/interests/:userId",
+  validateUserId(userIdSchema),
+  validateUpdateInterest(updateInterestsSchema),
+  updateInterest
+);
+
+// Delete interest
+
+/**
+ * @swagger
+ * /api/interests/{userId}:
+ *   delete:
+ *     summary: Delete user interests.
+ *     description: Delete the interests of a user.
+ *     tags:
+ *       - Interests
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         description: The ID of the user whose interests are to be deleted.
+ *         required: true
+ *         schema:
+ *           type: string
+ *         example: "550e8400-e29b-41d4-a716-446655440000"
+ *     responses:
+ *       200:
+ *         description: Interests details successfully deleted.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 successful:
+ *                   type: boolean
+ *                 message:
+ *                   type: string
+ *                 deletedInterest:
+ *                   type: object
+ *       500:
+ *         description: Failed to delete interests.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 successful:
+ *                   type: boolean
+ *                   example: false
+ *                 message:
+ *                   type: string
+ *                   example: "Could not delete interest."
+ *                 error:
+ *                   type: string
+ */
+
+router.delete(
+  "/interests/:userId",
+  validateUserId(userIdSchema),
+  deleteInterest
+);
 
 module.exports = router;
