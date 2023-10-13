bebe554e123e93ef02b61fb9fd83ec87bd931a50
ansman58 <javanslem@gmail.com>
2023-10-13 06:35:43 +0100

    update certificate

--- src/controllers/certificate.controller.ts
+++ src/controllers/certificate.controller.ts
@@ -1,11 +1,11 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { connectionSource as dataSource } from "../database/data-source";
 import { success, error } from "../utils";
 import { updateACertificate } from "../services/certificate.service";
-import { UpdateCertificateInterface } from "../interfaces/certification.interface"
+import { UpdateCertificateInterface } from "../interfaces/certification.interface";
 import { Certificate } from "../database/entity/model";
 import { User } from "../database/entity/user";
-// import { validateUpdateData } from "../middlewares/certificate.zod"; // received an error when I used this
+import { validateCertificateData } from "../middlewares/certificate.zod";
 
 const certificateRepo = dataSource.getRepository(Certificate);
 
@@ -14,8 +14,8 @@ const addCertificateController = async (req: Request, res: Response) => {
     const { title, year, organization, url, description, sectionId } = req.body;
     const userId = req.params.userId;
 
-    // Check if the user with userId exists
     const userRepository = dataSource.getRepository(User);
+    // Check if the user with userId exists
     const isExistingUser = await userRepository.findOneBy({ id: userId });
 
     if (!isExistingUser) {
@@ -33,18 +33,22 @@ const addCertificateController = async (req: Request, res: Response) => {
       sectionId,
     };
 
-    // Create the certificate entity
-    const certificate = certificateRepo.create(certificateInfo);
+    const certificateDataIsValid = await validateCertificateData(req, res);
 
-    // Save the certificate to the database
-    const savedCertificate = await certificateRepo.save(certificate);
+    if (certificateDataIsValid) {
+      // Save the certificate to the database
+      const savedCertificate = await certificateRepo.save(certificateInfo);
 
-    if (!savedCertificate) {
-      return error(res, "Error creating certificate", 400);
+      if (!savedCertificate) {
+        return error(res, "Error creating certificate", 400);
+      }
+
+      return success(
+        res,
+        { ...savedCertificate },
+        "Certificate created successfully"
+      );
     }
-
-    // Include the certId in the response
-    return success(res, { ...savedCertificate }, "Certificate created successfully");
   } catch (err) {
     console.error("Error creating certificate:", err);
     return error(res, (err as Error)?.message);
@@ -60,30 +64,30 @@ const getAllCertificates = async (req: Request, res: Response) => {
     if (certificates.length > 0) {
       res.json(certificates);
     } else {
-      res.status(404).json({ error: 'No certificates found' });
+      res.status(404).json({ error: "No certificates found" });
     }
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
 const getCertificateById = async (req: Request, res: Response) => {
-  const  id  = req.params.certId;
+  const id = req.params.certId;
   const certificateRepository = dataSource.getRepository(Certificate);
 
   try {
     const certificate = await certificateRepository
-    .createQueryBuilder()
-    .where('id = :id', { id })
-    .getOne();
+      .createQueryBuilder()
+      .where("id = :id", { id })
+      .getOne();
 
     if (certificate) {
       res.json(certificate);
     } else {
-      res.status(404).json({ error: 'Certificate not found' });
+      res.status(404).json({ error: "Certificate not found" });
     }
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
@@ -91,14 +95,13 @@ const deleteCertificate = async (req: Request, res: Response) => {
   const id = req.params.certId;
   const certificateRepository = dataSource.getRepository(Certificate);
 
-  console.log('Request received to delete certificate with certId:', id);
-
+  console.log("Request received to delete certificate with certId:", id);
 
   try {
     const certificate = await certificateRepository
-    .createQueryBuilder()
-    .where('id = :id', { id })
-    .getOne();
+      .createQueryBuilder()
+      .where("id = :id", { id })
+      .getOne();
 
     if (certificate) {
       await certificateRepository.remove(certificate);
@@ -107,73 +110,75 @@ const deleteCertificate = async (req: Request, res: Response) => {
       const updatedCertificates = await certificateRepository.find();
 
       res.json({
-        message: 'Certificate deleted successfully',
-        certificates: updatedCertificates, 
+        message: "Certificate deleted successfully",
+        certificates: updatedCertificates,
       });
     } else {
-      res.status(404).json({ error: 'Certificate not found' });
+      res.status(404).json({ error: "Certificate not found" });
     }
   } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
+const uuidPattern =
+  /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
 
-const uuidPattern = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
-
-const isValidCertificate = (payload: any): payload is UpdateCertificateInterface => {
-   return (
-     typeof payload.title === "string" &&
-     typeof payload.year === "string" &&
-     typeof payload.organization === "string" &&
-     typeof payload.url === "string" &&
-     typeof payload.description === "string"
-   );
+const isValidCertificate = (
+  payload: any
+): payload is UpdateCertificateInterface => {
+  return (
+    typeof payload.title === "string" &&
+    typeof payload.year === "string" &&
+    typeof payload.organization === "string" &&
+    typeof payload.url === "string" &&
+    typeof payload.description === "string"
+  );
 };
- 
+
 const updateCertificate = async (req: Request, res: Response) => {
-   try {
-      const id = parseInt(req.params.id)
-      const userId = req.params.userId
-      const payload = req.body
+  try {
+    const id = parseInt(req.params.id);
+    const userId = req.params.userId;
+    const payload = req.body;
 
-      if (!id || typeof id !== "number" || !userId || !uuidPattern.test(userId)) {
-        return res.status(400).json({
-          success: false, 
-          message: "Please provide an integer id and a valid UUID user id as parameters"
-        })
-      }
+    if (!id || typeof id !== "number" || !userId || !uuidPattern.test(userId)) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "Please provide an integer id and a valid UUID user id as parameters",
+      });
+    }
 
-      if (!isValidCertificate(payload)) {
-        return res.status(400).json({
-          success: false,
-          message: "Payload should be in the format below",
-          format: {
-            title: "string",
-            year: "string",
-            organization: "string",
-            url: "string",
-            description: "string",
-          }
-        });
-      };
+    if (!isValidCertificate(payload)) {
+      return res.status(400).json({
+        success: false,
+        message: "Payload should be in the format below",
+        format: {
+          title: "string",
+          year: "string",
+          organization: "string",
+          url: "string",
+          description: "string",
+        },
+      });
+    }
 
-      const data = await updateACertificate(id, userId, payload)
-      if (data.successful) {
-         success(res, data.data[0], "Certificate updated successfully")
-      } else {
-         error(res, data.message);
-      }
-   } catch (error: any) {
-      return error(res, error.message)
-   }
-}
+    const data = await updateACertificate(id, userId, payload);
+    if (data.successful) {
+      success(res, data.data[0], "Certificate updated successfully");
+    } else {
+      error(res, data.message);
+    }
+  } catch (error: any) {
+    return error(res, error.message);
+  }
+};
 
 export {
   addCertificateController,
   deleteCertificate,
   getCertificateById,
   getAllCertificates,
-  updateCertificate
-}
-
+  updateCertificate,
+};
