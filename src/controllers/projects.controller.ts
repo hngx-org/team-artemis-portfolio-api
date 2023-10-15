7601a1305392e5501afa6441c4777696a6b3a4c2
Erickb <wr.t8r1@gmai.com>
2023-10-15 01:40:41 -0700

    Fix Project controllere bug

--- src/controllers/projects.controller.ts
+++ src/controllers/projects.controller.ts
@@ -48,8 +48,23 @@ export const getProjectById: RequestHandler = async (
 ) => {
   try {
     const { id } = req.params;
-    const data = await projectRepository.findOneBy({ id: +id });
-    success(res, data);
+    const project = await projectRepository.findOne({
+      where: {
+        id: +id,
+      },
+      relations: ['projectsImages'],
+    });
+    const allThumbnails = await projectImageRepository.find({ where: { project } });
+
+    const thumbnail = await imageRepository.findOne({ where: { id: allThumbnails[0].id } });
+
+    project.thumbnail = thumbnail.url as any;
+    const imageUrlsPromises = project.projectsImages.map(async (image) => {
+      const imageEntity = await imageRepository.findOne({ where: { id: image.id } });
+      return imageEntity ? imageEntity.url : null;
+    });
+    project.projectsImages = await Promise.all(imageUrlsPromises) as any;
+    success(res, project, "Successfully Retrieved");
   } catch (err) {
     error(res, (err as Error).message);
   }
@@ -128,13 +143,8 @@ export const createProject: RequestHandler = async (
       throw new BadRequestError('You can only upload a maximum of 10 images');
     }
 
-    // const imagesRes = await cloudinaryService(files, req.body.service);
-    const imagesRes = {
-      urls: [
-        'https://res.cloudinary.com/ol4juwon/image/upload/v1697351080/test/ggogjzsezo6fwzod5re7.png',
-        'https://res.cloudinary.com/ol4juwon/image/upload/v1697351082/test/mwcv2fp9yis7tmh8b1no.jpg'
-      ]
-    }
+    const imagesRes = await cloudinaryService(files, req.body.service);
+
 
     for (const url of imagesRes.urls) {
       const image = new Images() as Images;
@@ -173,12 +183,22 @@ export const createProject: RequestHandler = async (
         { id: newProject.id },
         { thumbnail: thumbnailId }
       );
-      const updatedProject = await projectRepository.findOneBy({
-        id: +newProject.id,
+
+      const updatedProject = await projectRepository.findOne({
+        where: {
+          id: +newProject.id,
+        },
+        relations: ['projectsImages'],
       });
+      updatedProject.thumbnail = thumbnail.url as any;
+      const imageUrlsPromises = updatedProject.projectsImages.map(async (image) => {
+        const imageEntity = await imageRepository.findOne({ where: { id: image.id } });
+        return imageEntity ? imageEntity.url : null;
+      });
+      updatedProject.projectsImages = await Promise.all(imageUrlsPromises) as any;
       success(res, updatedProject, "Successfully created");
     } else {
-      success(res, data, "Created without thumbnail");
+      success(res, { thumbnail: thumbnail.url, ...data }, "Created without thumbnail");
     }
   } catch (err) {
     return next(err);
