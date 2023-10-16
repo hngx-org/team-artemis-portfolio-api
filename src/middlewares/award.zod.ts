8a2f0922a342a079aec374b15383f804e0e5f28e
highb33kay <hiibeekayvibe@gmail.com>
2023-10-16 16:29:09 +0100

    Awards, Work Experience, Reference.

--- src/middlewares/award.zod.ts
+++ src/middlewares/award.zod.ts
@@ -1,8 +1,8 @@
-import { ZodError, z } from 'zod'
-import { NextFunction, Request, Response } from 'express'
-import { BadRequestError } from '../middlewares'
-import { parseAsync, ErrorMessageOptions } from 'zod-error'
-import { validate as isUUID } from 'uuid'
+import { ZodError, z } from "zod";
+import { NextFunction, Request, Response } from "express";
+import { BadRequestError } from "../middlewares";
+import { parseAsync, ErrorMessageOptions } from "zod-error";
+import { validate as isUUID } from "uuid";
 
 export const CreateAwardDataSchema = z.object({
   title: z.string(),
@@ -10,11 +10,9 @@ export const CreateAwardDataSchema = z.object({
   presented_by: z.string(),
   url: z.string().optional(),
   userId: z.string().refine((value) => isUUID(value), {
-    message: 'userId has to be a valid UUID',
+    message: "userId has to be a valid UUID",
   }),
-  section_id: z.number(),
-})
-
+});
 
 export const UpdateAwardDataSchema = z.object({
   title: z.string(),
@@ -22,9 +20,9 @@ export const UpdateAwardDataSchema = z.object({
   presented_by: z.string(),
   url: z.string().optional(),
   userId: z.string().refine((value) => isUUID(value), {
-    message: 'userId has to be a valid UUID',
-  })
-})
+    message: "userId has to be a valid UUID",
+  }),
+});
 
 // Custom function to validate date strings in "yyyy" format
 function validateDateYYYY(dateString: string) {
@@ -34,11 +32,11 @@ function validateDateYYYY(dateString: string) {
 
 const options: ErrorMessageOptions = {
   delimiter: {
-    error: ' 🔥 ',
+    error: " 🔥 ",
   },
   transform: ({ errorMessage, index }) =>
     `Error #${index + 1}: ${errorMessage}`,
-}
+};
 
 async function validateCreateAwardData(
   req: Request,
@@ -46,56 +44,63 @@ async function validateCreateAwardData(
   next: NextFunction
 ) {
   try {
-    const data = req.body
+    const data = req.body;
 
     // Validate date strings in "yy-mm-dd" format
     if (data.year && !validateDateYYYY(data.year)) {
-      const err = new BadRequestError("Invalid 'year' date format, it must be 'yyyy' ")
-      return res.status(err.statusCode).json({ error: err.message })
+      const err = new BadRequestError(
+        "Invalid 'year' date format, it must be 'yyyy' "
+      );
+      return res.status(err.statusCode).json({ error: err.message });
     }
 
     // Retrieve the "userId" from request parameters
-    const userId = req.params.userId
+    const userId = req.params.userId;
 
     // Validate the rest of the data against the schema
     const result = await parseAsync(CreateAwardDataSchema, {
       ...data,
       userId,
-      options
-    })
+      options,
+    });
 
     // Store the validated data in the request object if needed
-    const validatedData = result
-    console.log(validatedData)
-    next() // Continue to the next middleware or route handler
+    const validatedData = result;
+    console.log(validatedData);
+    next(); // Continue to the next middleware or route handler
   } catch (error) {
-    const err = new BadRequestError(error.message)
-    console.log(err.message)
-    res.status(err.statusCode).json({ error: err.message })
+    const err = new BadRequestError(error.message);
+    console.log(err.message);
+    res.status(err.statusCode).json({ error: err.message });
   }
 }
 
-async function validateUpdateAwardData(req: Request, res: Response, next: NextFunction) {
+async function validateUpdateAwardData(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   try {
     const data = req.body;
 
     // Validate date strings in "yyyy" format
     if (data.year && !validateDateYYYY(data.year)) {
-      const err = new BadRequestError("Invalid 'year' date format, it must be 'yyyy'");
+      const err = new BadRequestError(
+        "Invalid 'year' date format, it must be 'yyyy'"
+      );
       return res.status(err.statusCode).json({ error: err.message });
     }
 
     // Validate the data against the schema
     const result = await parseAsync(UpdateAwardDataSchema, data, options);
 
-    
     const validatedData = result;
-    console.log(validatedData)
-    next(); 
+    console.log(validatedData);
+    next();
   } catch (error) {
     const err = new BadRequestError(error.message);
     console.error(err.message);
     res.status(err.statusCode).json({ error: err.message });
   }
 }
-export { validateCreateAwardData, validateUpdateAwardData }
+export { validateCreateAwardData, validateUpdateAwardData };
--- src/middlewares/reference.zod.ts
+++ src/middlewares/reference.zod.ts
@@ -16,11 +16,7 @@ export const CreateReferenceDetailSchema = object({
       message: "Company is required in the request body",
     }),
 
-  position: string()
-    .min(1, { message: "Position must not be an empty string" })
-    .refine((position) => !!position, {
-      message: "Position is required in the request body",
-    }),
+  position: string().optional(),
 
   emailAddress: string()
     .min(1, { message: "Email address must not be an empty string" })
@@ -28,18 +24,13 @@ export const CreateReferenceDetailSchema = object({
       message: "Email address is required in the request body",
     }),
 
-  phoneNumber: string()
-    .min(1, { message: "Phone number must not be an empty string" })
-    .refine((phoneNumber) => !!phoneNumber, {
-      message: "Phone number is required in the request body",
-    }),
+  phoneNumber: string().optional(),
 
   userId: string()
     .min(1, { message: "User ID must not be an empty string" })
     .optional()
     .nullish(),
 });
-
 
 // Custom function to validate email addresses
 function validateEmail(email: string) {
@@ -58,25 +49,28 @@ async function validateCreateReferenceData(
     }
     // Retrieve the "userId" from request parameters
     let userId = req.params.id;
-      if (!userId) {
-        userId = req.body.userId;
-      }
-      
+    if (!userId) {
+      userId = req.body.userId;
+    }
+
     // Validate the rest of the data against the schema
     const result = await parseAsync(CreateReferenceDetailSchema, {
-      ...data, userId,
+      ...data,
+      userId,
     });
-    
+
     // Store the validated data in the request object if needed
     const validatedData = result;
     next(); // Continue to the next middleware or route handler
   } catch (error) {
-      if (error instanceof ZodError) {
-          const errorMessages = (error.issues as ZodIssue[]).map((issue) => issue.message);
-          res.status(400).json({ errors: errorMessages });
-      } else {
-          next(error);
-      }
+    if (error instanceof ZodError) {
+      const errorMessages = (error.issues as ZodIssue[]).map(
+        (issue) => issue.message
+      );
+      res.status(400).json({ errors: errorMessages });
+    } else {
+      next(error);
+    }
   }
 }
 
