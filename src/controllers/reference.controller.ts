8605a6d84e515c7fa4a3732852621bb76f925f54
thereson <gerardokolie09@gmail.com>
2023-10-20 09:18:04 -0700

    created new validation function

--- src/controllers/reference.controller.ts
+++ src/controllers/reference.controller.ts
@@ -102,44 +102,71 @@ export const deleteReferenceDetail = async (
   }
 };
 
+// export const updateReference = async (req: Request, res: Response) => {
+//   try {
+//     const { id }: any = req.params;
+
+//     let fields = Object.keys(req.body);
+
+//     let shouldContinue = true;
+//     let message: string;
+
+//     for (let i = 0; i < fields.length; i++) {
+//       const field = fields[i];
+//       console.log(field);
+//       if (field == "sectionId") continue;
+
+//       if (!isNaN(Number(req.body[field]))) {
+//         message = field;
+//         shouldContinue = false;
+//       }
+//     }
+
+//     if (!shouldContinue) {
+//       return error(res, `${message} should be a string`, 422);
+//     }
+
+//     await connectionSource
+//       .createQueryBuilder()
+//       .update(ReferenceDetail)
+//       .set(req.body)
+//       .where("id = :id", { id: id })
+//       .execute();
+
+//     const userRepository = connectionSource.getRepository(ReferenceDetail);
+//     const refByid = await userRepository.findOneBy({
+//       id: id,
+//     });
+
+//     success(res, refByid);
+//   } catch (err) {
+//     error(res, "invalid reference id");
+//   }
+// };
+
 export const updateReference = async (req: Request, res: Response) => {
   try {
+    updatereferenceschema.parse(req.body);
+    if (JSON.stringify(req.body) === "{}") {
+      return error(res, "cannot send an empty Request Body");
+    }
     const { id }: any = req.params;
-
-    let fields = Object.keys(req.body);
-
-    let shouldContinue = true;
-    let message: string;
-
-    for (let i = 0; i < fields.length; i++) {
-      const field = fields[i];
-      console.log(field);
-      if (field == "sectionId") continue;
-
-      if (!isNaN(Number(req.body[field]))) {
-        message = field;
-        shouldContinue = false;
-      }
-    }
-
-    if (!shouldContinue) {
-      return error(res, `${message} should be a string`, 422);
-    }
-
     await connectionSource
       .createQueryBuilder()
       .update(ReferenceDetail)
       .set(req.body)
       .where("id = :id", { id: id })
       .execute();
-
     const userRepository = connectionSource.getRepository(ReferenceDetail);
     const refByid = await userRepository.findOneBy({
       id: id,
     });
-
     success(res, refByid);
   } catch (err) {
-    error(res, "invalid reference id");
+    if (err instanceof z.ZodError) {
+      error(res, err.flatten().fieldErrors);
+    } else {
+      error(res, "invalid Reference ID");
+    }
   }
 };
