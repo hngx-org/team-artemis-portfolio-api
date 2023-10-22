8c7b1f6d4fd4539dbc53b9e610caf9c8a0f5e925
Erickb <wr.t8r1@gmai.com>
2023-10-22 02:47:36 -0700

    Add shop service to retrieve all shops of a user

--- src/routes/shop.route.ts
+++ src/routes/shop.route.ts
@@ -8,19 +8,19 @@ import {
 
 /**
  * @swagger
- * /api/v1/sections/{userId}/shops:
+ * /api/v1/sections/{slug}/shops:
  *   post:
- *     summary: Create a new about details for a user.
- *     description: Create a new about details for a user by providing the user's ID.
+ *     summary: Create a new shop section for a user.
+ *     description: Create a new shop section for a user by providing the user's ID.
  *     parameters:
  *       - in: path
- *         name: userId
- *         description: The ID of the user for whom to create an about.
+ *         name: slug
+ *         description: The ID of the user for whom to create an shop.
  *         required: true
  *         type: string
  *       - in: body
- *         name: createAbout
- *         description: About data to be created
+ *         name: createshop
+ *         description: shop data to be created
  *         required: true
  *         schema:
  *           type: object
@@ -30,11 +30,11 @@ import {
  *             section_id:
  *               type: integer
  *         example:
- *           bio: "New About details"
+ *           bio: "New shop section"
  *           section_id: 54
  *     responses:
  *       '201':
- *         description: About details successfully created.
+ *         description: shop section successfully created.
  *         content:
  *           application/json:
  *             schema:
@@ -46,14 +46,14 @@ import {
  *                   type: string
  *                 statusCode:
  *                   type: integer
- *                 about:
+ *                 shop:
  *                   type: object
  *             example:
- *               message: "About details created successfully"
+ *               message: "shop section created successfully"
  *               status: "Success"
  *               statusCode: 201
- *               about:
- *                 bio: "New About details"
+ *               shop:
+ *                 bio: "New shop section"
  *                 section_id: 3
  *       '400':
  *         description: Bad request
@@ -89,10 +89,98 @@ import {
  *             example:
  *               'Input Error': "Internal server error"
  *     tags:
- *       - About
+ *       - shop
  */
 
+
 router.post("/sections/:user_slug/shops", createShopSection);
+
+/**
+ * @swagger
+ * /api/v1/sections/{slug}/shops:
+ *   get:
+ *     summary: get a shop section for a user.
+ *     description: get shop section for a user by providing the user's ID.
+ *     parameters:
+ *       - in: path
+ *         name: slug
+ *         description: The ID of the user for whom to create an shop.
+ *         required: true
+ *         type: string
+ *       - in: body
+ *         name: createshop
+ *         description: shop data to be created
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             bio:
+ *               type: string
+ *             section_id:
+ *               type: integer
+ *         example:
+ *           bio: "New shop section"
+ *           section_id: 54
+ *     responses:
+ *       '201':
+ *         description: shop section successfully created.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 status:
+ *                   type: string
+ *                 statusCode:
+ *                   type: integer
+ *                 shop:
+ *                   type: object
+ *             example:
+ *               message: "shop section created successfully"
+ *               status: "Success"
+ *               statusCode: 201
+ *               shop:
+ *                 bio: "New shop section"
+ *                 section_id: 3
+ *       '400':
+ *         description: Bad request
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 'Input Error':
+ *                   type: string
+ *             example:
+ *               'Input Error': "Invalid input data"
+ *       '404':
+ *         description: Not Found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 'Input Error':
+ *                   type: string
+ *             example:
+ *               'Input Error': "User not found"
+ *       '500':
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 'Input Error':
+ *                   type: string
+ *             example:
+ *               'Input Error': "Internal server error"
+ *     tags:
+ *       - shop
+ */
+
 
 router.get("/sections/:user_slug/shops", getUserShopSection);
 
